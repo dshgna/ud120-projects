9ecec505e488c863d81515e7ae432d930ff2e656
Dulshani Gunawardhana <degunawardhana@gmail.com>
2016-01-13 21:04:51 +0530

    Final project: data exploration

---
+++ final_project/original.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 12 14:42:00 2016
+
+@author: DULEE
+"""
+
+#!/usr/bin/python
+
+import sys
+import pickle
+import matplotlib
+sys.path.append("../tools/")
+
+from feature_format import featureFormat, targetFeatureSplit
+from tester import dump_classifier_and_data
+
+### Task 1: Select what features you'll use.
+### features_list is a list of strings, each of which is a feature name.
+### The first feature must be "poi".
+financial_features = ['salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses', 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 'director_fees']
+email_features = ['to_messages', 'email_address', 'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi', 'poi', 'shared_receipt_with_poi']
+features_list = ['poi', 'salary'] # You will need to use more features
+
+print "No of data points:", len()
+
+
+### Load the dictionary containing the dataset
+with open("final_project_dataset.pkl", "r") as data_file:
+    data_dict = pickle.load(data_file)
+
+### Task 2: Remove outliers
+
+
+
+def outlier_visualization(data):
+    for point in data:
+        f1 = point[0]
+        f2 = point[1]
+        matplotlib.pyplot.scatter(f1, f2 )
+    
+    matplotlib.pyplot.xlabel("Feature 1")
+    matplotlib.pyplot.ylabel("Feature 2")
+    matplotlib.pyplot.show()
+
+
+### Task 3: Create new feature(s)
+### Store to my_dataset for easy export below.
+my_dataset = data_dict
+
+### Extract features and labels from dataset for local testing
+data = featureFormat(my_dataset, features_list, sort_keys = True)
+labels, features = targetFeatureSplit(data)
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.3, random_state=42)
+
+### Task 4: Try a varity of classifiers
+### Please name your classifier clf for easy export below.
+### Note that if you want to do PCA or other multi-stage operations,
+### you'll need to use Pipelines. For more info:
+### http://scikit-learn.org/stable/modules/pipeline.html
+
+# Provided to give you a starting point. Try a variety of classifiers.
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+### using our testing script. Check the tester.py script in the final project
+### folder for details on the evaluation method, especially the test_classifier
+### function. Because of the small size of the dataset, the script uses
+### stratified shuffle split cross validation. For more info: 
+### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+
+# Example starting point. Try investigating other evaluation techniques!
+'''
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.3, random_state=42)
+'''
+### Task 6: Dump your classifier, dataset, and features_list so anyone can
+### check your results. You do not need to change anything below, but make sure
+### that the version of poi_id.py that you submit can be run on its own and
+### generates the necessary .pkl files for validating your results.
+
+dump_classifier_and_data(clf, my_dataset, features_list)
--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+from __future__ import division
+import pprint
+import matplotlib
 
 import sys
 import pickle
@@ -10,46 +13,99 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+financial_features = ['salary', 'total_payments', 'bonus', 'deferred_income', 'total_stock_value', 'expenses', 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock']
+email_features = ['to_messages', 'email_address', 'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi', 'poi', 'shared_receipt_with_poi']
+features_list = ['poi', 'salary'] # You will need to use more features
+pp = pprint.PrettyPrinter(indent=4)
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
-### Task 2: Remove outliers
-### Task 3: Create new feature(s)
-### Store to my_dataset for easy export below.
+# Explore dataset
+data_points = len(data_dict)
+poi_count = 0
+non_poi_count = 0
+# check for missing values
+missing_value_map = { 'bonus': {'count':0, 'poi':0}, 'deferral_payments': {'count':0, 'poi':0},
+    'deferred_income': {'count':0, 'poi':0},'director_fees': {'count':0, 'poi':0}, 
+    'exercised_stock_options': {'count':0, 'poi':0}, 'total_payments': {'count':0, 'poi':0},
+    'expenses': {'count':0, 'poi':0}, 'loan_advances': {'count':0, 'poi':0},
+    'long_term_incentive': {'count':0, 'poi':0}, 'restricted_stock_deferred': {'count':0, 'poi':0},
+    'other': {'count':0, 'poi':0}, 'restricted_stock': {'count':0, 'poi':0}, 
+    'total_stock_value': {'count':0, 'poi':0}, 'salary': {'count':0, 'poi':0}, 
+    'email_address': {'count':0, 'poi':0}, 'from_messages': {'count':0, 'poi':0}, 
+    'from_poi_to_this_person': {'count':0, 'poi':0}, 'shared_receipt_with_poi': {'count':0, 'poi':0},
+    'from_this_person_to_poi': {'count':0, 'poi':0}, 'to_messages': {'count':0, 'poi':0} }
+
+
+for person, features in data_dict.iteritems():    
+    isPoi = False    
+    if features['poi'] == True:
+        poi_count += 1
+        isPoi = True
+    else:
+        non_poi_count += 1
+    for name, value in features.iteritems():         
+        if value == 'NaN':
+            missing_value_map[name]['count'] += 1
+            if isPoi:
+                missing_value_map[name]['poi'] += 1
+
+
+print "Data points:\t", data_points 
+print "no of POIs:\t\t", poi_count
+print "no of non POIs:\t", non_poi_count
+print "POI ratio:\t\t", poi_count/data_points
+print "Total features:\t", len(data_dict[data_dict.keys()[0]])
+print "Financial features:\t", len(financial_features)
+print "Email features:\t", len(email_features)
+
+significant_missing_values = []
+significant_poi_values = []
+
+#print "{:<25} {:<20} {:<10}".format('Feature','missing','poi')
+for feature, values in missing_value_map.iteritems():
+    missing_ratio = values['count']/data_points
+    if missing_ratio > 0.5:
+        significant_missing_values.append(feature)
+    poi_ratio = values['poi']/poi_count
+    if poi_ratio > 0.5:
+        significant_poi_values.append(feature)
+    #print "{:<25} {:<20} {:<10}".format(feature, values['count'], values['poi'])
+
+print "Features with >50% missing values:", significant_missing_values
+print "Features with pois with >50% missing values:", significant_poi_values
+
 my_dataset = data_dict
+
+### Task 2: Remove outliers
+data_outliers = featureFormat(my_dataset, financial_features)
+labels_outliers, features_outliers = targetFeatureSplit(data_outliers)
+
+
+def outlier_visualization(data):
+    for point in data:
+        f1 = point[0]
+        f2 = point[1]
+        matplotlib.pyplot.scatter(f1, f2 )
+    
+    matplotlib.pyplot.xlabel("Feature 1")
+    matplotlib.pyplot.ylabel("Feature 2")
+    matplotlib.pyplot.show()
+
+
 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
-### Task 4: Try a varity of classifiers
-### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
 
-# Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
-### using our testing script. Check the tester.py script in the final project
-### folder for details on the evaluation method, especially the test_classifier
-### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
-# Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
 
-### Task 6: Dump your classifier, dataset, and features_list so anyone can
-### check your results. You do not need to change anything below, but make sure
-### that the version of poi_id.py that you submit can be run on its own and
-### generates the necessary .pkl files for validating your results.
 
-dump_classifier_and_data(clf, my_dataset, features_list)
+
+
+
+
